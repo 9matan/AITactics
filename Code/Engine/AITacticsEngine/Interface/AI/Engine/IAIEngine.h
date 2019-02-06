a9a82a983850eee749058b40254f145ec79b4d9b
Oleh Yavorovych <oleg.yavorovich@gmail.com>
2019-02-06 14:16:02 +0200

    Added AIEngine interface

---
+++ Code/Engine/AITacticsEngine/Interface/AI/Engine/IAIEngine.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace AITactics
+{
+
+class IAIAgentsManager;
+class IAIBrainsManager;
+struct SAIEngineParams;
+
+class IAIEngine
+{
+public:
+    virtual void Initialize(SAIEngineParams const& engineParams) = 0;
+    virtual void Shutdown() = 0;
+
+    virtual void Update(float const deltaTime) = 0;
+
+    virtual IAIAgentsManager& GetAgentsManager() = 0;
+    virtual IAIBrainsManager& GetBrainsManager() = 0;
+};
+
+} // namespace AITactics
